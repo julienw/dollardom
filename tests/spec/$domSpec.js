6134313c735c805a50e997f80b04a41a7359e1ea
Julien Wajsberg <felash@gmail.com>
2011-06-02 23:44:21 +0200

    more tests

--- tests/spec/$domSpec.js
+++ tests/spec/$domSpec.js
@@ -53,10 +53,10 @@ describe("$dom", function() {
         expect(nodes.length).toEqual(1);
         expect(nodes).toContain(testElements.sib2);
     });
-    it("by class and id", function() {
+    it("not by class and id", function() {
         var nodes = $dom.get('.sib2#middle');
-        expect(nodes.length).toEqual(1);
-        expect(nodes).toContain(testElements.sib2);
+        expect(nodes.length).toEqual(0); // we don't support id after classes
+        expect(nodes).toEqual([]);
     });
 
     it("by direct children", function() {
@@ -114,11 +114,11 @@ describe("$dom", function() {
         expect(newNode.id).toEqual("element");
         expect(newNode.className).toEqual("class");
     });
-    it("can create elements with class and id", function() {
+    it("can not create elements with class and id", function() {
         var newNode = $dom.create('div.class#element');
         expect(newNode instanceof Element).toBeTruthy();
         expect(newNode.localName).toEqual("div");
-        expect(newNode.id).toEqual("element");
+        expect(newNode.id).not.toEqual("element"); // we don't support id after classes
         expect(newNode.className).toEqual("class");
     });
   });
@@ -208,6 +208,63 @@ describe("$dom", function() {
       });
   });
 
+  describe("descendants", function() {
+      it("find general descendants", function() {
+          var nodes = $dom.descendants(testElements.tests, "span");
+          expect(nodes.length).toEqual(3);
+          expect(nodes).toContainM(testElements.level3, testElements.sib2, testElements.sib3);
+      });
+      it("find direct descendants", function() {
+          var nodes = $dom.descendants(testElements.tests, "> span");
+          expect(nodes.length).toEqual(2);
+          expect(nodes).toContainM(testElements.sib2, testElements.sib3);
+      });
+      it("find more complex selectors", function() {
+          var nodes = $dom.descendants(testElements.tests, ".level1 span");
+          expect(nodes.length).toEqual(1);
+          expect(nodes).toContainM(testElements.level3);
+      });
+  });
+
+  describe("next", function() {
+      it("find next", function() {
+          var node = $dom.next(testElements.sib1);
+          expect(node).toBe(testElements.sib2);
+      });
+      it("find next again", function() {
+          var node = $dom.next(testElements.sib2);
+          expect(node).toBe(testElements.sib3);
+      });
+      it("find next with selector", function() {
+          var node = $dom.next(testElements.sib1, ".sib3");
+          expect(node).toBe(testElements.sib3);
+      });
+      it("find next with selector id", function() {
+          var node = $dom.next(testElements.sib1, "#middle");
+          expect(node).toBe(testElements.sib2);
+      });
+  });
+
+
+  describe("previous", function() {
+      it("find previous", function() {
+          var node = $dom.previous(testElements.sib2);
+          expect(node).toBe(testElements.sib1);
+      });
+      it("find previous again", function() {
+          var node = $dom.previous(testElements.sib3);
+          expect(node).toBe(testElements.sib2);
+      });
+      it("find previous with selector", function() {
+          var node = $dom.previous(testElements.sib3, ".sib1");
+          expect(node).toBe(testElements.sib1);
+      });
+      it("find previous with selector id", function() {
+          var node = $dom.previous(testElements.sib3, "#middle");
+          expect(node).toBe(testElements.sib2);
+      });
+  });
+
   describe("manipulating CSS", function() {
       it("hasClass", function() {
           expect($dom.hasClass(testElements.level1, "level1")).toBeTruthy();
@@ -220,6 +277,10 @@ describe("$dom", function() {
           expect(testElements.level1.className).toMatch(/\btest\b/);
           expect(testElements.level1.className).toMatch(/\blevel1\b/);
           expect(testElements.level1.className).toMatch(/\bsib1\b/);
+      });
+      it("removeClass", function() {
+          $dom.removeClass(testElements.level1, "sib1");
+          expect(testElements.level1.className).not.toMatch(/\bsib1\b/);
       });
   });
 });
