80a79dd34b93db8b25e5ddbd1a801764d0ba55fc
Julien Wajsberg <felash@gmail.com>
2011-07-11 23:36:12 +0200

    fixed completely broken examples

--- examples/examples.js
+++ examples/examples.js
@@ -10,25 +10,26 @@ function init() {
 }
 
 function setupExamples() {
-	var button = document.getElementById('create');
-	$dom.addEvent(button, "click", function() {
-		var result = $dom.next(button, ".result");
+	var button1 = document.getElementById('create');
+	$dom.addEvent(button1, "click", function() {
+		var result = $dom.next(button1, ".result");
 		$dom.empty(result);
 		
 		var node = $dom.create('div.panel.red');
 		result.appendChild(node);
 	});
 	
-	button = document.getElementById('animate');
-	$dom.addEvent(button, 'click', function() {
-		var result = $dom.next(button, ".result");
+	var button2 = document.getElementById('animate');
+	$dom.addEvent(button2, 'click', function() {
+		var result = $dom.next(button2, ".result");
 		var square = $dom.first(result.firstChild, ".square");
 		
 		$dom.transform(square, { left: "150px", opacity: 0.2 });
 	});
-	button = document.getElementById('animate-reset');
-	$dom.addEvent(button, 'click', function() {
-		var result = $dom.next(button, ".result");
+	
+	var button3 = document.getElementById('animate-reset');
+	$dom.addEvent(button3, 'click', function() {
+		var result = $dom.next(button3, ".result");
 		var square = $dom.first(result.firstChild, ".square");
 		
 		$dom.transform(square, { left: 0, opacity: 1 });
